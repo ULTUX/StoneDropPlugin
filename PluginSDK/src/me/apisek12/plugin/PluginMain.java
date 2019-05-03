1595f6fd299ac8e2308e608d20b5594173735c1e
W Nowak <bulion@zoho.com>
2019-05-03 14:42:43 +0200

    removed usless functions

--- PluginSDK/src/me/apisek12/plugin/PluginMain.java
+++ PluginSDK/src/me/apisek12/plugin/PluginMain.java
@@ -5,16 +5,9 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.security.Permission;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
 
 public class PluginMain extends JavaPlugin {
     static Plugin plugin = null;
@@ -34,12 +27,6 @@ public class PluginMain extends JavaPlugin {
 
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            if (command.getName().equalsIgnoreCase("kol") && player.isOp()) {
-                player.getInventory().addItem(new ItemStack(Material.TNT));
-                player.getInventory().addItem(new ItemStack(Material.FLINT_AND_STEEL));
-                player.sendTitle(ChatColor.RED + "Dodano TNT do ekwipunku!", ChatColor.AQUA + "oraz zapalniczke...", 10, 40, 20);
-                return true;
-            }
             if (command.getName().equalsIgnoreCase("toon") && args != null){
                 Collection<Player> arrayList = (Collection<Player>) Bukkit.getOnlinePlayers();
                 Player playerSelected = null;
@@ -55,16 +42,16 @@ public class PluginMain extends JavaPlugin {
 //                if (jesli == true){
 //                    getServer().broadcastMessage(player.getDisplayName()+": "+args[1]);
 //                }
-            }
-            if (command.getName().equalsIgnoreCase("sethome")) {
-                location = player.getLocation();
-                player.sendTitle(ChatColor.RED + "Changed home location.", "", 10, 40, 20);
-            }
-            if (command.getName().equalsIgnoreCase("home")){
-                player.teleport(location);
-                player.sendTitle(ChatColor.GREEN + "Teleported!", "", 10, 40, 20);
-                player.getLocation().getWorld().spawnParticle(Particle.EXPLOSION_HUGE, location, 2);
-            }
+        }
+//            if (command.getName().equalsIgnoreCase("sethome")) {
+//                location = player.getLocation();
+//                player.sendTitle(ChatColor.RED + "Changed home location.", "", 10, 40, 20);
+//            }
+//            if (command.getName().equalsIgnoreCase("home")){
+//                player.teleport(location);
+//                player.sendTitle(ChatColor.GREEN + "Teleported!", "", 10, 40, 20);
+//                player.getLocation().getWorld().spawnParticle(Particle.EXPLOSION_HUGE, location, 2);
+//            }
         } else if (sender instanceof ConsoleCommandSender)
             sender.getServer().getConsoleSender().sendMessage("To musi byc gracz a nie konsola!");
         return false;
